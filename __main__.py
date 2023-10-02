cfbfc0c3e4dc62ad22f344045ac162cbf73749fb
Madhan <madhankumaravelu93@gmail.com>
2023-10-02 09:24:03 -0500

    cert auth resources

--- __main__.py
+++ __main__.py
@@ -1,4 +1,6 @@
 import compute_resources
+import certificate_authority
 import pulumi
 
 pulumi.export("kubernetesPublicAddress", compute_resources.load_balancer.dns_name)
+pulumi.export("kubernetesPublicAddress", certificate_authority.result)
