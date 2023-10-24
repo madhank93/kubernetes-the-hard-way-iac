0f80bab135304d68cb34d1da0ba5d418ced29560
Madhan <madhankumaravelu93@gmail.com>
2023-10-24 14:00:07 -0500

    feat: adding dns add on feature

--- __main__.py
+++ __main__.py
@@ -1,5 +1,8 @@
-import compute_resources
-
 import pulumi
+import compute_resources
+import pod_network_routes
+import dns_addon
 
 pulumi.export("Kubernetes-Public-Address", compute_resources.load_balancer.dns_name)
+pulumi.export("route-table", pod_network_routes.route_tables)
+pulumi.export("dns-output", dns_addon.config.resources)
