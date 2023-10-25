88a0a3c409b900d5b01640a5b0f5dc0933217b0e
Madhan <madhankumaravelu93@gmail.com>
2023-10-25 15:36:12 -0500

    feat: adding smoke test - nginx deployment

--- __main__.py
+++ __main__.py
@@ -2,7 +2,9 @@ import pulumi
 import compute_resources
 import pod_network_routes
 import dns_addon
+import smoke_test
 
 pulumi.export("Kubernetes-Public-Address", compute_resources.load_balancer.dns_name)
 pulumi.export("route-table", pod_network_routes.route_tables)
-pulumi.export("dns-output", dns_addon.config.resources)
+pulumi.export("dns-output", dns_addon.core_dns._name)
+pulumi.export("nginx-srv-url", smoke_test.nginx_url)
